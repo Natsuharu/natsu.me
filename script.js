2ea119c4ab686bcddc50a47e2dd268fb26ec572c
Natsuharu <>
2023-05-15 09:36:51 +0900

    <fix>ヘッダーの背景を設定、メニュー部分の作成

---
+++ script.js
@@ -0,0 +1,27 @@
+// 背景
+bubbly({
+  colorStart: "#D9E5FF",
+  colorStop: "#005FFF",
+  canvas: document.querySelector("#back"),
+});
+
+// let current = 1;
+// let next = 2;
+
+// let max_count = 2;
+
+// $(function () {
+//   let timer;
+//   timer = window.setInterval(animation, 1000);
+// });
+
+// function animation() {
+//   $("#HAM_" + next).fadeIn(500);
+//   $("#HAM_" + current).fadeOut(500);
+
+//   current = next;
+//   next++;
+//   if (next > max_count) {
+//     next = 1;
+//   }
+// }
